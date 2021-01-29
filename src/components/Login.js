03be9ed502fa00bcf971a0fc82a45d85e2d7c37e
Nathan Hietala <Nrhietala@gmail.com>
2021-01-29 10:13:50 -0600

    codegrade test

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,8 @@ const intialCredentials = {
   password:"",
 }
 
+// codegrade test 1
+
 const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
