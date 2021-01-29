e6d6f17c2d98d53aafa965f67d908ad4de081f76
Nathan Hietala <Nrhietala@gmail.com>
2021-01-29 12:50:56 -0600

    RTL test done

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -2,18 +2,46 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
+import {axiosWithAuth as mockaxiosWithAuth} from '../helpers/axiosWithAuth';
+
+jest.mock('../helpers/axiosWithAuth')
+
+const dummyColors =[
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff",
+    },
+    id: 1,
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  }
+]
+
+
 test("Renders BubblePage without errors", () => {
   render(<BubblePage />);
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
-  // Arange: Render page 
-  render(<BubblePage />)
+test("Fetches data and renders the bubbles on mounting", async () => {
+  const { rerender } = (<BubblePage colorList={dummyColors} />)
 
-  // Act
+  render(<BubblePage colorList={[]} />)
 
-  
-  // Assert
+  expect(colorList).toHaveLength(0)
+
+  mockaxiosWithAuth.mockResolvedValueOnce(dummyColors)
+
+  rerender(<BubblePage colorList={dummyColors}/>)
+
+
+
+  expect(colorList).toHaveLength(2)
 });
 
 //Task List
