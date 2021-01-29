e45194e3235fe82a1181cf69beda97deca13e0b0
Nathan Hietala <Nrhietala@gmail.com>
2021-01-29 10:35:37 -0600

    Pulling in data from API to render bubbles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import PrivateRoute from './utils/PrivateRoute';
@@ -7,9 +7,6 @@ import "./styles.scss";
 import BubblePage from "./components/BubblePage";
 
 function App() {
-  const [ colorList, setColorList ] = useState([]);
-
-
   return (
     <Router>
       <div className="App">
